89bc81c68b85422fa60209b528691d006bc12119
Neil Berkman <neil@berkman.com>
2021-04-12 08:03:08 -0700

    bump version

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule FindSiteIcon.MixProject do
   use Mix.Project
 
-  @version "0.3.8"
+  @version "0.3.9"
   @url "https://github.com/XukuLLC/find_site_icon"
   @maintainers [
     "Neil Berkman"
