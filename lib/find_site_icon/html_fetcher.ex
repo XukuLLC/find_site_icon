fff080e7a58fa57b80fb6298ecdc59a4c1441b82
Neil Berkman <neil@berkman.com>
2023-01-31 13:09:47 -0800

    Handle HTML fetching errors more robustly

--- lib/find_site_icon/html_fetcher.ex
+++ lib/find_site_icon/html_fetcher.ex
@@ -4,8 +4,13 @@ defmodule FindSiteIcon.HTMLFetcher do
   alias FindSiteIcon.Util.HTTPUtils
 
   def fetch_html(url) do
-    with {:ok, %Tesla.Env{status: 200, body: html}} <- HTTPUtils.do_get(url) do
-      {:ok, html}
+    try do
+      with {:ok, %Tesla.Env{status: 200, body: html}} <- HTTPUtils.do_get(url) do
+        {:ok, html}
+      end
+    rescue
+      # Return error tuple
+      err -> {:error, Exception.message(err)} || {:error, :unknown}
     end
   end
 end
